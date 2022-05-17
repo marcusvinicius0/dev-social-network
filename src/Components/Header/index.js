a8b5ce47559453d5a3996cd8f2f5c9144efd1fa8
Marcus Vinícius <begheli2020@gmail.com>
2022-05-17 20:10:04 -0300

    searchbar done with filter

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/auth'
 
 import { AiOutlineHome } from 'react-icons/ai'
@@ -7,7 +7,7 @@ import { BsFillChatDotsFill } from 'react-icons/bs'
 import { FiSettings, FiLogOut, FiSearch } from 'react-icons/fi'
 import { BiSearchAlt } from 'react-icons/bi';
 
-// import { UsersResults } from '../UsersResults'
+import { UsersResults } from '../UsersResults'
 
 import { UsersContext } from '../../contexts/users'
 
@@ -21,9 +21,45 @@ export function Header() {
     const { user, signOut } = useContext(AuthContext);
     const { users } = useContext(UsersContext);
     const [search, setSearch] = useState("");
-    const [showResults, setShowResults] = useState(false);
+    const [searchBarBox, setSearchBarBox] = useState(false);
+    const [userFilters, setUserFilters] = useState([]);
 
-   
+    function usersFilter(value){
+        
+        let filterUsers = []
+
+        users.forEach((item) => {
+            if(String(item.name).toLowerCase().includes(value.toLowerCase())){
+                filterUsers.push(item)
+                setUserFilters(filterUsers)
+                
+            }
+        })
+      
+    }
+
+    function hiddingContent() {
+
+        if (searchBarBox !== false) {
+            return (
+                <ul>
+                    {userFilters.map(item => (
+                        <Link onClick={() => {}} to={`/users/${item.id}`} key={item.id}> <FiSearch /> {item.name} <img src={item.avatarUrl} /> </Link>
+                    ))}
+                    <Link className="linkToSeeMoreResults" to="/">Ver mais resultados</Link>
+                </ul>
+            )
+        } else{
+            return (
+                ""
+            )
+        }
+    }
+
+    function searchValue(e){ //alterando valor do input
+        setSearch(e.target.value)
+        usersFilter(e.target.value)
+    }
 
     return (
         <>
@@ -35,14 +71,16 @@ export function Header() {
 
                         <span className="search-bar">
 
-                            <button><BiSearchAlt color="rgba(0, 0, 0, 0.8)" size={23} /></button>
-                            <input type="text" placeholder="Pesquisar..." value={search} onChange={(e) => setSearch(e.target.value)}
-                                onClick={() => setShowResults(!showResults)}
+                            <BiSearchAlt color="rgba(0, 0, 0, 0.8)" size={23} />
+                            <input type="text" placeholder="Pesquisar..." value={search} onChange={(e) => searchValue(e)}
+                                onClick={() => setSearchBarBox(!searchBarBox)}
                             />
-
-
+                            <div onMouseLeave={() => setSearchBarBox(!searchBarBox)} className={searchBarBox ? "searchBoxOn" : "searchBoxOff"}>
+                                {hiddingContent()}
+                            </div>
                         </span>
                     </div>
+
 
                     <nav className="optionsNav">
 
--- src/Components/UsersResults/index.js
+++ src/Components/UsersResults/index.js
@@ -1,31 +1,30 @@
-// import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext } from 'react'
 
-// import { UsersContext } from '../../contexts/users'
+import { UsersContext } from '../../contexts/users'
 
-// import './style.css'
+import './style.css'
 
-// // import firebase from '../../services/firebaseConnection'
+// import firebase from '../../services/firebaseConnection'
 
-// // import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-// // import { FiSearch } from 'react-icons/fi'
+import { FiSearch } from 'react-icons/fi'
 
-// export function UsersResults() {
-//     const { users } = useContext(UsersContext)
-//     const [search, setSearch] = useState('');
+export function UsersResults() {
+    const { users } = useContext(UsersContext)
+    const [search, setSearch] = useState('');
+    
+    return(
+        <>
+            <article className="boxUserResults">
+                <ul>
+                    {users.map( item => (
+                        <Link to={`/users/${item.id}`} key={item.id}> <FiSearch /> {item.name} <img src={item.avatarUrl} /> </Link>
+                    ))}
 
-
-//     return(
-//         <>
-//             <article className="boxUserResults">
-//                 <ul>
-//                     {users.map( item => (
-//                         <Link to={`/users/${item.id}`} key={item.id}> <FiSearch /> {item.name} <img src={item.avatarUrl} /> </Link>
-//                     ))}
-
-//                     <Link className="linkToSeeMoreResults" to="/">Ver mais resultados</Link>
-//                 </ul>
-//             </article>
-//         </>
-//     )
-// }
+                    <Link className="linkToSeeMoreResults" to="/">Ver mais resultados</Link>
+                </ul>
+            </article>
+        </>
+    )
+}
